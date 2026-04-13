44c8aa4e4cd051222aec38834c32888069888556
meshidenn <mesitahiro@hotmail.com>
2026-04-13 09:03:30 +0900

    change env

--- tech/papers/hf-paper-tracker/scripts/cron_env.sh
+++ tech/papers/hf-paper-tracker/scripts/cron_env.sh
@@ -4,5 +4,5 @@
 export PATH="${HOME}/.local/bin:${HOME}/.local/share/mise/shims:/usr/local/bin:/usr/bin:/bin${PATH:+:$PATH}"
 
 # Obsidian vault へ生成 md をコピーする際の宛先（上書き可能）
-export OBSIDIAN_HF_PAPERS_DAILY="${OBSIDIAN_HF_PAPERS_DAILY:-$HOME/Obsidian/hf-papers/daily}"
-export OBSIDIAN_HF_PAPERS_WEEKLY="${OBSIDIAN_HF_PAPERS_WEEKLY:-$HOME/Obsidian/hf-papers/weekly}"
+export OBSIDIAN_HF_PAPERS_DAILY="${OBSIDIAN_HF_PAPERS_DAILY:-$HOME/Obsidian/intake/hf-papers/daily}"
+export OBSIDIAN_HF_PAPERS_WEEKLY="${OBSIDIAN_HF_PAPERS_WEEKLY:-$HOME/Obsidian/intake/hf-papers/weekly}"
